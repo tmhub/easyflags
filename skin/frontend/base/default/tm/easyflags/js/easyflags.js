2040d066fd5cedf4d4d6eb6e863ef76119248558
Vitaliy <vitaliy@mage2cloud.com>
2016-12-12 18:11:18 +0200

    frontend value and dropdown complete

--- skin/frontend/base/default/tm/easyflags/js/easyflags.js
+++ skin/frontend/base/default/tm/easyflags/js/easyflags.js
@@ -2,27 +2,54 @@ var EasyFlagsSelect = Class.create();
 EasyFlagsSelect.prototype = {
 
     initialize: function (select, options = {}) {
+        this.valTemplate = new Template(
+            '<img src="#{flag}" class="flag"/>#{value}'
+        );
+        this.optTemplate = new Template(
+            '<img src="#{flag}" class="flag"/><span>#{value}</span>'
+        );
         this.chosen = new Chosen(select, options);
-        this.chosen.result_add_option = this.wrapResultAddOption();
+        this.assignValueFlag();
+        this.wrapResultAddOption();
     },
 
     wrapResultAddOption: function () {
-        return this.chosen.result_add_option.wrap(
+        this.chosen.result_add_option = this.chosen.result_add_option.wrap(
             function (originalMethod, option) {
-                this.assignFlag(option);
-                return originalMethod(option);
+                var html = originalMethod(option);
+                if (html) {
+                    html = this.assignOptionFlag(option, html);
+                }
+                return html;
             }.bind(this)
         );
     },
 
-    assignFlag: function (opt) {
+    assignValueFlag: function () {
+        var selectedOptions = this.chosen.form_field.selectedOptions;
+        if (selectedOptions.length > 0) {
+            var data = {
+                value: selectedOptions[0].innerHTML,
+                flag: selectedOptions[0].getAttribute('data-flag-url')
+            };
+            this.chosen.container.down('.chosen-single span').innerHTML =
+                this.valTemplate.evaluate(data);
+        }
+    },
+
+    assignOptionFlag: function (opt, html) {
+        var tempElement = new Element('div').update(html);
+        var li = tempElement.down('li');
+        var data = {
+            value: li.innerHTML
+        }
         // assign flag image to dropdown item
         var options = this.chosen.form_field.options;
         if (typeof options[opt.array_index] !== 'undefined') {
-            var imgUrl = options[opt.array_index].getAttribute('data-flag-url');
-            opt.search_text = '<img src="' + imgUrl + '">'
-                + '<span>' + opt.search_text + '</span>';
+            data.flag = options[opt.array_index].getAttribute('data-flag-url');
         }
+        li.innerHTML = this.optTemplate.evaluate(data);
+        return tempElement.innerHTML;
     }
 
 };
@@ -30,6 +57,6 @@ EasyFlagsSelect.prototype = {
 document.observe("dom:loaded", function(){
     window.easyFlags = new EasyFlagsSelect(
         $('select-language'),
-        {width: "100px", disable_search_threshold: 10}
+        {width: "150px", disable_search_threshold: 10}
     );
 });
